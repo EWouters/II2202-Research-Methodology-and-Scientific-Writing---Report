1edfe051aa71d160c5b8f2194705fac96bfe6871
erik <6179932+EWouters@users.noreply.github.com>
2018-10-25 16:19:48 +0200

    started work on a python memory allocation visualizer simulator class.

--- nvvp/profile_export.sh
+++ nvvp/profile_export.sh
@@ -2,12 +2,12 @@
 
 FILE="/home/erik/anaconda3/bin/python"
 WORKING_DIR="/home/erik/GPU_Malloc/benchmarks/scripts/tf_cnn_benchmarks/"
-ARGUMENTS="/home/erik/GPU_Malloc/benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --num_gpus=1 --batch_size=16 --model=resnet50 --variable_update=parameter_server"
+ARGUMENTS="/home/erik/GPU_Malloc/benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --num_gpus=1 --batch_size=16 --model=resnet50 --variable_update=parameter_server --num_batches=1 --num_warmup_batches=0"
 EXPORT_DIR="/home/erik/GPU_Malloc/nvvp"
 
 cd ${WORKING_DIR}
 
-nvprof --export-profile ${EXPORT_DIR}/timeline.prof ${FILE} ${ARGUMENTS}
-nvprof --metrics achieved_occupancy,executed_ipc -o ${EXPORT_DIR}/metrics.prof ${FILE} ${ARGUMENTS}
-nvprof --source-level-analysis pc_sampling -o ${EXPORT_DIR}/pcsampling.prof ${FILE} ${ARGUMENTS}
-nvprof --analysis-metrics -o ${EXPORT_DIR}/analysis_metrics.prof ${FILE} ${ARGUMENTS}
+nvprof -f --export-profile ${EXPORT_DIR}/timeline.prof ${FILE} ${ARGUMENTS}
+nvprof -f --metrics achieved_occupancy,executed_ipc -o ${EXPORT_DIR}/metrics.prof ${FILE} ${ARGUMENTS}
+nvprof -f --source-level-analysis pc_sampling -o ${EXPORT_DIR}/pcsampling.prof ${FILE} ${ARGUMENTS}
+nvprof -f --analysis-metrics -o ${EXPORT_DIR}/analysis_metrics.prof ${FILE} ${ARGUMENTS}
